e60e9a1df6e484f9ae311d6c965ae692d26b98b4
Vertado <vkzheny@gmail.com>
2020-03-13 10:27:49 +0300

    wiki

---
+++ lab_4/lab_4/lab_4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    bool t = true, f = false;
+
+    char a = 'A';
+
+    wchar_t ch = 'А';
+    
+    short X = 24;  // f9
+    short X1 = -24;
+
+    short big = 0x8000;
+    short small = 0xffffffffffff8000;
+}
