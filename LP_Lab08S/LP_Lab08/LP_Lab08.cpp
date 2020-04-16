696ad4dd087febb79e3dcceec4295983c87beef3
Vertado <vkzheny@gmail.com>
2020-04-16 21:07:02 +0300

    Complete nahuy

---
+++ LP_Lab08S/LP_Lab08/LP_Lab08.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "../LP_Lab08L/Dictionary.h"
+
+#pragma comment(lib, "D:\\cpp_projs\\PL\\LP_Lab08S\\Debug\\LP_Lab08L.lib ")
+
+using std::cout;
+using std::endl;
+using namespace Dictionary;
+
+int main()
+{
+	try {
+		setlocale(LC_ALL, "ru");
+
+		Instance d1 = Create("Преподаватели", 5);
+		Entry e1 = { 1, "Гладкий" }, e2 = { 2, "Веялкин" }, e3 = { 3, "Смелов" }, e4 = { 4, "Урбанович" }, e5 = { 2, "Пацей" };
+		AddEntry(d1, e1);
+		AddEntry(d1, e2);
+		AddEntry(d1, e3);
+		AddEntry(d1, e4);
+		Entry ex2 = GetEntry(d1, 4);
+		DelEntry(d1, 2);
+		Entry newentry1 = { 6, "Гурин" };
+		UpdEntry(d1, 3, newentry1);
+		Print(d1);
+
+		Instance d2 = Create("Студенты", 30);
+		Entry s1 = { 1, "Иванов" }, s2 = { 2, "Петров" }, s3 = { 3, "Сидоров" };
+		AddEntry(d2, s1);
+		AddEntry(d2, s2);
+		AddEntry(d2, s3);
+		Entry newentry3 = { 4, "Николаев" };
+		UpdEntry(d2, 3, newentry3);
+		Print(d2);
+		Delete(d1);
+		Delete(d2);
+	}
+	catch (char* e) {
+		cout << e << endl;
+	}
+}
---
+++ LP_Lab08S/LP_Lab08L/LP_Lab08L.cpp
@@ -0,0 +1,100 @@
+﻿// LP_Lab08L.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+#include <iostream>
+#include "Dictionary.h"
+
+using namespace Dictionary;
+
+Instance Dictionary::Create(const char name[DICTNAMEMAXSIZE], int size)
+{
+	if (strlen(name) > DICTNAMEMAXSIZE)
+		throw THROW01;
+
+	if (size > DICTMAXSIZE)
+		throw THROW02;
+	Instance dict;
+	strcpy_s(dict.name, name);
+	dict.size = 0;
+	dict.maxsize = size;
+
+	dict.dictionary = new Entry[size];
+
+	return dict;
+}
+
+void Dictionary::AddEntry(Instance& inst, Entry ed) {
+	if (inst.size == inst.maxsize)
+		throw THROW03;
+	for (int i = 0; i < inst.size; i++)
+		if (inst.dictionary[i].id == ed.id)
+			throw THROW04;
+
+	inst.dictionary[inst.size] = ed;
+
+	inst.size++;
+}
+
+void Dictionary::DelEntry(Instance& inst, int id) {
+	bool found = false;
+	int elNum = 0;
+	for (int i = 0; i < inst.size; i++)
+		if (inst.dictionary[i].id == id)
+			elNum = i;
+
+	if (!elNum)
+		throw THROW05;
+
+	for (int i = elNum; i < inst.maxsize; i++)
+		inst.dictionary[i] = inst.dictionary[i + 1];
+
+	inst.size--;
+
+}
+
+void Dictionary::UpdEntry(Instance& inst, int id, Entry new_ed) {
+	int elNum = 0;
+
+	for (int i = 0; i < inst.size; i++)
+		if (inst.dictionary[i].id == id)
+			elNum = i;
+
+	if (!elNum)
+		throw THROW07;
+
+	for (int i = 0; i < inst.size; i++)
+		if (inst.dictionary[i].id == new_ed.id)
+			throw THROW08;
+
+	inst.dictionary[elNum] = new_ed;
+}
+
+Entry Dictionary::GetEntry(Instance inst, int id) {
+	int elNum = 0;
+
+	for (int i = 0; i < inst.size; i++)
+		if (inst.dictionary[i].id == id) {
+			elNum = i;
+		}
+
+
+	if (elNum == 0)
+		throw THROW05;
+
+	return inst.dictionary[elNum];
+}
+
+void Dictionary::Print(Instance inst) {
+	printf("--------- %s ---------\n", inst.name);
+	for (int i = 0; i < inst.size; i++)
+		printf("%d %s\n", inst.dictionary[i].id, inst.dictionary[i].name);
+}
+
+void Dictionary::Delete(Instance& d)
+{
+	d.maxsize = 0;
+	d.size = 0;
+}
